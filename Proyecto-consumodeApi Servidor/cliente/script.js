13c2842ec91fce1b1a24bc45d868e971e4d961d3
Melissa García <88354500+MeliiG@users.noreply.github.com>
2023-09-17 16:22:28 -0500

    Añadir retardo - spinner

--- Proyecto-consumodeApi Servidor/cliente/script.js
+++ Proyecto-consumodeApi Servidor/cliente/script.js
@@ -1,6 +1,13 @@
 // Función para cargar y mostrar los datos desde el archivo JSON
 async function cargarDatos() {
     try {
+
+        const spinner = document.getElementById('spinner');
+        spinner.style.display = 'block'; // Mostrar el spinner
+
+        // Simular un retraso de 2 segundos (2000 ms)
+        await new Promise(resolve => setTimeout(resolve, 3000));
+
         // Hacer una solicitud para cargar el archivo JSON local
         const response = await fetch('nuevodb.json');
         const jsonData = await response.json();
@@ -17,6 +24,7 @@ async function cargarDatos() {
             `;
             tableBody.appendChild(row);
         });
+        spinner.style.display = 'none'; // Ocultar el spinner después de cargar los datos
     } catch (error) {
         console.error('Error al cargar los datos desde el archivo JSON:', error);
     }
